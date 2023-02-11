69f988be8e1315b89e655dd16989d260a6d335b3
Brenner <augustinbrenner@gmail.com>
2023-02-11 14:23:35 +0100

    thèmeJeuIPO.txt

---
+++ Zuul/Site/index.php
@@ -0,0 +1,26 @@
+<?php
+
+// URL de base pour les routes
+$base_url = '/';
+
+// Tableau associatif qui mappe les URL avec les noms de vues
+$routes = [
+  '' => 'home.php',
+  'auteur' => 'auteur.php',
+];
+
+// Récupère le chemin d'URL après la base
+$path = str_replace($base_url, '', $_SERVER['REQUEST_URI']);
+
+// Si la route demandée est trouvée dans le tableau de routes, utilisez la vue associée
+if (array_key_exists($path, $routes)) {
+  $view = 'views/' . $routes[$path];
+  if (file_exists($view)) {
+    include $view;
+    exit;
+  }
+}
+
+// Sinon, renvoie une erreur 404
+http_response_code(404);
+include 'views/404.php';
---
+++ Zuul/Site/views/template/template.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://img.icons8.com/windows/32/null/pirates-of-the-caribbean.png">
+    <title><?= $GLOBALS['title'] ?></title>
+  </head>
+  <header>
+      <div class="logo">Logo</div>
+      <div class="bandeau">Bandeau</div>
+  </header>
+  <body>
+    <?php require("views/template/navbar.php"); ?>
+    <?= $GLOBALS['content'] ?>
+
+    <footer>
+      <?php // require("view/footer.php");?>
+
+    </footer>
+
+  </body>
+
+</html>
